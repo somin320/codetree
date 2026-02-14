cc1a7d0baf3b73793b112336eb5323a7083bdc8f
somin320 <166691005+somin320@users.noreply.github.com>
2026-02-14 14:25:05 +0900

    Title: 굉장한 수, Time: 74ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260214/굉장한 수/amazing-number.py
+++ 260214/굉장한 수/amazing-number.py
@@ -1,4 +1,4 @@
 N = int(input())
-if (N // 3 == 0 and N // 1 == 0 ) or (N // 5 == 0 and N // 2 == 0 ): 
-    print("True")
-else: print("False")
+if (N % 3 == 0 and N % 2 == 1 ) or (N % 5 == 0 and N  2 == 0 ): 
+    print("true")
+else: print("false")
