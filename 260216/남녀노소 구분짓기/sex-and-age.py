c75721cad0fe9169bbf8fdde9f4b11ebb04cf65e
somin320 <166691005+somin320@users.noreply.github.com>
2026-02-16 23:54:00 +0900

    Title: 남녀노소 구분짓기, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260216/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,13 @@
+age = int(input())
+sex = input()
+
+if age >= 19:
+    if sex = 1:
+        print("Woman")
+    elif sex = 0:
+        print("Man")
+else:
+    if sex = 1:
+        print("Girl")
+    elif sex = 0:
+        print("Boy")
