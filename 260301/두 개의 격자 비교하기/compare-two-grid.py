f9b861900f9e7ba1c0a1790560ecfd7f60d0104d
somin320 <166691005+somin320@users.noreply.github.com>
2026-03-01 23:58:27 +0900

    Title: 두 개의 격자 비교하기, Time: 94ms, Memory: 21MB, Status: Runtime Error - Codetree

---
+++ 260301/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,13 @@
+rows = 4
+cols = 4
+matrix1 = [list(map(int, input().split())) for _ in range(rows)]
+matrix2 = [list(map(int, input().split())) for _ in range(rows)]
+
+for i in range(rows):
+    new_row = []
+    for j in range(cols):
+        if matrix1[i][j] == matrix2[i][j]:
+            new_row.append(0)
+        else:
+            new_row.append(1)
+    print(*new_row)
